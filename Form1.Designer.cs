a8ac39ffec921ede97e5176ff7f84d44ee8ace44
Unknown Coder <ksy@vs21.kr>
2022-07-11 16:10:05 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace BackGroundWorker_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uiBtn_Create = new System.Windows.Forms.Button();
+            this.uiPrg_FileCreate = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uiBtn_Create
+            // 
+            this.uiBtn_Create.Location = new System.Drawing.Point(12, 26);
+            this.uiBtn_Create.Name = "uiBtn_Create";
+            this.uiBtn_Create.Size = new System.Drawing.Size(776, 171);
+            this.uiBtn_Create.TabIndex = 0;
+            this.uiBtn_Create.Text = "Run";
+            this.uiBtn_Create.UseVisualStyleBackColor = true;
+            this.uiBtn_Create.Click += new System.EventHandler(this.uiBtn_Create_Click);
+            // 
+            // uiPrg_FileCreate
+            // 
+            this.uiPrg_FileCreate.Location = new System.Drawing.Point(12, 224);
+            this.uiPrg_FileCreate.Name = "uiPrg_FileCreate";
+            this.uiPrg_FileCreate.Size = new System.Drawing.Size(776, 23);
+            this.uiPrg_FileCreate.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 279);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.uiPrg_FileCreate);
+            this.Controls.Add(this.uiBtn_Create);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uiBtn_Create;
+        private System.Windows.Forms.ProgressBar uiPrg_FileCreate;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackGroundWorker_Test
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker _worker = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //폼 Load 이벤트
+            this.Load += FormLoad_Event;
+
+            //버튼 클릭 이벤트
+            uiBtn_Create.Click += uiBtn_Create_Click;
+
+            //폼 Close 이벤트
+            this.FormClosing += FormClosing_Event;
+        }
+
+        /// <summary>
+        /// 폼 Load 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void FormLoad_Event(object sender, EventArgs e)
+        {
+            //백그라운드 객체 생성 및 스레드 시작
+            InitBackgroundWorker();
+        }
+
+        /// <summary>
+        /// 버튼 클릭 이벤트 핸들러
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void uiBtn_Create_Click(object sender, EventArgs e)
+        {
+            //작업 쓰레드가 Busy(즉, 실행중이 아닌상태) 라면
+            if (_worker.IsBusy != true)
+            {
+                uiPrg_FileCreate.Maximum = RollCount;
+                //비동기로 실행
+                _worker.RunWorkerAsync();
+            }
+        }
+
+        /// <summary>
+        /// 백그라운드 스레드 생성 및 이벤트 선언
+        /// </summary>
+        public void InitBackgroundWorker()
+        {
+            _worker = new BackgroundWorker();
+            _worker.WorkerReportsProgress = true;
+            _worker.WorkerSupportsCancellation = true;
+            _worker.DoWork += new DoWorkEventHandler(Worker_DoWork);
+            _worker.ProgressChanged += new ProgressChangedEventHandler(Worker_ProgressChanged);
+            _worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(Worker_RunWorkerComplete);
+        }
+
+        int RollCount = 10000;
+
+        /// <summary>
+        /// 작업 쓰레드가 실제로 하는 일
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string tmpDir = @"C:\Temp\Create_{0}";
+
+            int cnt = 0;
+            int pct = 0;
+
+            try
+            {
+                for (int idx = 1; idx <= RollCount; idx++)
+                {
+                    //이미 파일이 있다면
+                    if (Directory.Exists(tmpDir.Replace("{0}", idx.ToString())))
+                    {
+                        //해당 파일 지우고
+                        Directory.Delete(tmpDir.Replace("{0}", idx.ToString()));
+                    }
+
+                    //다시 생성
+                    Directory.CreateDirectory(tmpDir.Replace("{0}", idx.ToString()));
+
+                    //pct = ((++cnt * 100) / idx);
+                    pct = idx;
+
+                    _worker.ReportProgress(pct);
+                }
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// Progress 리포트 - UI 쓰레드
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.uiPrg_FileCreate.Value = e.ProgressPercentage;
+        }
+
+        /// <summary>
+        /// 작업 완료 - UI 쓰레드
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Worker_RunWorkerComplete(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //Error 체크
+            if (e.Error != null)
+            {
+                string msg = string.Format("Errpr : {0}", e.Error.Message);
+                MessageBox.Show(msg);
+                return;
+            }
+            else
+            {
+                string msg = string.Format("파일 생성 성공");
+                MessageBox.Show(msg);
+            }
+        }
+
+        /// <summary>
+        /// 폼이 닫히면
+        /// 실행중이던 BackGroundWorker 스레드 종료
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void FormClosing_Event(object sender, EventArgs e)
+        {
+            //_worker 객체가 생성된게 있다면
+            if (_worker != null)
+            {
+                //해당 스레드 객체가 Busy 상태이면
+                if (_worker.IsBusy)
+                {
+                    //스레드 취소
+                    _worker.CancelAsync();
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"테스트~!");
+        }
+    }
+}
